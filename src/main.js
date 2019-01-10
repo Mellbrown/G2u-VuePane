e6223c0628ebfe6ac9d061eafd0fb8dd1f76a442
mellbrown <2mellbrown@gmail.com>
2019-01-10 21:38:52 +0900

    packages I use a lot.

--- src/main.js
+++ src/main.js
@@ -3,6 +3,16 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+import 'bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+library.add(fas)
+Vue.component('icon', FontAwesomeIcon)
+
 Vue.config.productionTip = false
 
 new Vue({
